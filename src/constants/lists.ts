4d0f033c20489e5206199d577fd6141729ecb3e5
baby <elont842@gmail.com>
2024-12-11 04:50:29 +0800

    Update lists.ts

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,4 +1,4 @@
 // the Uniswap Default token list lives here
-export const DEFAULT_TOKEN_LIST_URL = 'https://raw.githubusercontent.com/sdzczh/tokenlist/master/default'
+export const DEFAULT_TOKEN_LIST_URL = 'https://raw.githubusercontent.com/ElonTrumpGo/FusionSwap/main/src/constants/Token.json'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [DEFAULT_TOKEN_LIST_URL]
