4c11c8917e644a9818a2d5a63bb5d571a3693c86
baby <elont842@gmail.com>
2024-12-11 04:45:14 +0800

    done

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,4 +1,4 @@
 // the Uniswap Default token list lives here
-export const DEFAULT_TOKEN_LIST_URL = 'tokens.uniswap.eth'
+export const DEFAULT_TOKEN_LIST_URL = 'https://raw.githubusercontent.com/sdzczh/tokenlist/master/default'
 
-export const DEFAULT_LIST_OF_LISTS: string[] = ['https://umaproject.org/uma.tokenlist.json']
+export const DEFAULT_LIST_OF_LISTS: string[] = [DEFAULT_TOKEN_LIST_URL]
